a0e879267ce5027551a97c64b89589aa5245b4fc
Carine <carine.rey@ens-lyon.fr>
2017-12-13 15:48:26 +0100

    add tests

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 test:
 	bash etc/test.sh
+test_det:
+	bash etc/test_det.sh
 
 build_docker:
 	bash etc/build_docker.sh
@@ -7,4 +9,4 @@ build_docker:
 push_docker:
 	bash etc/build_docker.sh push_yes
 
-.PHONY: test build_docker
+.PHONY: test build_docker test_det
--- etc/test.sh
+++ etc/test.sh
@@ -9,20 +9,21 @@ cd test
 ROOT_DIR=$PWD
 TREES_DIR=$ROOT_DIR/../etc/data/tree_ensembl
 OUTPUT_DIR=$ROOT_DIR/out_test/
+rm -r $OUTPUT_DIR || ls $ROOT_DIR
 mkdir -p  $OUTPUT_DIR
 
 
-meth="--topo --pcoc --ident"
+meth="--pcoc"
 debug="--no_cleanup --debug"
-debug=""
+#debug=""
 # test1: TOPO IDENT PCOC NO_NOISE
-docker run --rm -i -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test1 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1  $meth $debug"
+docker run --rm -i -e LOCAL_USER_ID=`id -u $USER` -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test1 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1  $meth $debug"
 
 #test2: TOPO IDENT PCOC BL_NOISE
-docker run --rm -i -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test2 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1 $meth $debug --bl_noise"
+docker run --rm -i -e LOCAL_USER_ID=`id -u $USER` -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test2 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1 $meth $debug --bl_noise"
 
 #test3: TOPO IDENT PCOC ALI_NOISE
-docker run --rm -i -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test3 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1  $meth $debug --ali_noise"
+docker run --rm -i -e LOCAL_USER_ID=`id -u $USER` -v $ROOT_DIR:$ROOT_DIR -v $TREES_DIR:$TREES_DIR  carinerey/pcoc  bash -c "pcoc_sim.py -td $TREES_DIR -o $OUTPUT_DIR/test3 -n_sc 1 -nb_sampled_couple 1 -n_sites 100 -c 5 -c_max 7 -cpu 1  $meth $debug --ali_noise"
 
 
 echo TOPO IDENT PCOC NO_NOISE
