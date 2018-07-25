0e1b9f62042dc6832cfc04478e39a8aed89b2b87
Carine Rey <carine.rey@ens-lyon.fr>
2018-07-25 15:23:04 +0200

    add a tag

--- etc/build_docker.sh
+++ etc/build_docker.sh
@@ -3,8 +3,9 @@
 set -e
 
 IMAGE_NAME=pcoc
+TAG=v1.0.0
 DOCKERFILE_DIR=.
-REPO=carinerey/$IMAGE_NAME
+REPO=carinerey/$IMAGE_NAME:$TAG
 docker build -t $REPO -f etc/Dockerfile $DOCKERFILE_DIR
 
 if [[ $1 == "push_yes" ]]
