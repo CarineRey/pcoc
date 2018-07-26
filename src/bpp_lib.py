6fbfbca84d45e7ff154c961a287f4fbdf2401113
Carine Rey <carine.rey@ens-lyon.fr>
2018-07-26 11:02:50 +0200

    add a warning message if no site is retained after the indel trimming and exit with 0

--- src/bpp_lib.py
+++ src/bpp_lib.py
@@ -221,8 +221,10 @@ def make_estim(name, c1, c2, g_tree, NBCATest=10, suffix="",
         logger.debug("%s\n%s", out, command)
 
     info_filename = "%s/%s_%s_%s%s.infos" %(repest, name , c1, c2, suffix)
-    if "Number of sites retained...............: 0" in out:
-        logger.warning("No site retained for %s", info_filename)
+
+    if "Number of sites retained...............: 0" in out or \
+       "Number of sites........................: 0" in out:
+        logger.warning("No site retained for %s (too much gaps), you can use the \"--max_gap_allowed\" option.", name)
         f_infos = open(info_filename,"w")
         f_infos.close()
 
